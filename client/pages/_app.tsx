bcf8a369e8309c84451ecc8a54fd0ba4a0ba54d7
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-09 12:06:49 -0600

    Adding cache using apollo client

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -7,7 +7,22 @@ import { ApolloProvider, ApolloClient, gql, InMemoryCache } from '@apollo/client
 
 const client = new ApolloClient({
   uri: "https://api.escuelajs.co/graphql",
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          product: {
+            read(_, {args, toReference}) {
+              return toReference({
+                __typename: "Product",
+                id: args?.id
+              })
+            }
+          }
+        }
+      }
+    }
+  })
 })
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
